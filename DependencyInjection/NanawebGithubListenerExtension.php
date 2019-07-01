6ca1102dff847f2a015353b11f1d0fcc74e303aa
77web <info@77-web.com>
2019-07-01 11:49:04 +0900

    githubのwebhookに存在する全イベントを定義

--- DependencyInjection/NanawebGithubListenerExtension.php
+++ DependencyInjection/NanawebGithubListenerExtension.php
@@ -17,7 +17,50 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 class NanawebGithubListenerExtension extends Extension
 {
     private $githubEvents = [
-
+        'check_run',
+        'check_suite',
+        'commit_comment',
+        'content_reference',
+        'create',
+        'delete',
+        'deploy_key',
+        'deployment',
+        'deployment_status',
+        'fork',
+        'github_app_authorization',
+        'gollum',
+        'installation',
+        'installation_repositories',
+        'issue_comment',
+        'issues',
+        'label',
+        'marketplace_purchase',
+        'member',
+        'membership',
+        'meta',
+        'milestone',
+        'organization',
+        'org_block',
+        'page_build',
+        'project_card',
+        'project_column',
+        'project',
+        'public',
+        'pull_request',
+        'pull_request_review',
+        'pull_request_review_comment',
+        'push',
+        'registry_package',
+        'release',
+        'repository',
+        'repository_import',
+        'repository_vulnerability_alert',
+        'security_advisory',
+        'star',
+        'status',
+        'team',
+        'team_add',
+        'watch',
     ];
 
     /**
@@ -31,6 +74,7 @@ class NanawebGithubListenerExtension extends Extension
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
 
+        $this->loadGithubListeners($container);
     }
 
     private function loadGithubListeners(ContainerBuilder $container)
